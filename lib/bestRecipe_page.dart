ac6782604731c184d70cb09c805180554812df9b
DIANA <yhu1@macalester.edu>
2026-04-07 10:58:59 -0500

    1

--- lib/bestRecipe_page.dart
+++ lib/bestRecipe_page.dart
@@ -4,20 +4,19 @@ import 'allRecipes_page.dart';
 
 class RecipePage extends StatefulWidget {
   final SpoonacularService service;
-  final List<dynamic> recipes;
   final Map<String, dynamic> bestRecipe;
   final List<String> haveIngredients;
   final List<String> needIngredients;
-  final List<String> initialIngredients;
+  final List<dynamic> recipes;
+
   //constructor
   const RecipePage({
     super.key,
     required this.service,
-    required this.recipes,
     required this.bestRecipe,
     required this.haveIngredients,
     required this.needIngredients,
-    required this.initialIngredients,
+    required this.recipes,
   });
 
   @override
@@ -31,11 +30,11 @@ class _RecipePageState extends State<RecipePage> {
       context,
       MaterialPageRoute(
         builder: (context) => ResultPage(
-          recipes: widget.recipes,
           service: widget.service,
+          bestRecipe: widget.bestRecipe,
+          recipes: widget.recipes,
           haveIngredients: widget.haveIngredients,
           needIngredients: widget.needIngredients,
-          initialIngredients: widget.initialIngredients,
         ),
       ),
     );
@@ -44,70 +43,39 @@ class _RecipePageState extends State<RecipePage> {
   @override
   Widget build(BuildContext context) {
     final recipe = widget.bestRecipe;
+    final allIngredients = recipe['extendedIngredients'] as List;
     final have = widget.haveIngredients;
     final need = widget.needIngredients;
     
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Your Best Recipe")),
-      body: Padding(padding: const EdgeInsets.all(25),
-      child: ListView(
-        children: [
-          Text(
-            recipe['title'],
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            "Ingredients you have:",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          ...have.map(
-            (ingredient) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Icon(Icons.check, size: 20, color: Colors.green),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      ingredient,
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 20),
-
-          const Text(
-            "Ingredients you need:",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          ...need.map(
-            (ingredient) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Icon(Icons.cancel, size: 20, color: Colors.red),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      ingredient,
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
+      appBar: AppBar(title: Text(recipe['title'])),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: ListView(
+          children: [
+            ...allIngredients.map((i) {
+              final name = i ['name'];
+              if (have.contains(name)) {
+                return Row(children: [
+                  const Icon(Icons.check, color: Colors.green),
+                  const SizedBox(width: 10),
+                  Text(name),
+                ]);
+              } else if (need.contains(name)) {
+              return Row(children: [
+                const Icon(Icons.cancel, color: Colors.red),
+                const SizedBox(width: 10),
+                Text(name),
+              ]);
+            } else {
+              return Row(children: [
+                const Icon(Icons.circle, size: 8),
+                const SizedBox(width: 10),
+                Text(name),
+              ]);
+            }
+          }),
           ElevatedButton(onPressed: seeMore, child: const Text("See More")),
         ],
       ),
