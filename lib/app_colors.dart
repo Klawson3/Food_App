0e26d83b86dd1c01e8d058fb7a11c1ed74ff14be
Spam Butler <scutler1@macalester.edu>
2026-04-12 21:22:52 -0500

    Added theme colors

---
+++ lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color deepSpinach = Color.fromRGBO(46, 92, 49, 1);
+  static const Color crispLettuce = Color.fromARGB(255, 155, 224, 81);
+  static const Color carrotOrange = Color.fromRGBO(255, 152, 0, 1);
+  static const Color fetaWhite = Color.fromRGBO(249, 251, 231, 1);
+  static const Color peppercorn = Color.fromRGBO(30, 33, 29, 1);
+}
