1457c1b1c028289318603acb36bc9edf59a4ebd4
Acacia Edmond <aedmond@macalester.edu>
2026-05-04 13:07:34 -0500

    startPage documentation

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -3,13 +3,21 @@ import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 import 'package:animate_do/animate_do.dart'; 
 
-//import 'diet_selection_page.dart';
 import 'key_ingredient_page.dart';
 
-//This is the first page that the user sees when they open the app. 
+/// start_page is the entry screen of the application.
+///
+/// It serves as the initial page where the user begins interacting with the app.
+/// The page displays a visually appealing background, the app title, and a
+/// primary button that navigates the user to the key ingredient page.
+///
+
+/// Displays the UI elements and handles navigation throughout the app.
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
+
+/// Builds the UI. Navigation is triggered when the user presses the start button. 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +47,8 @@ class StartPage extends StatelessWidget {
                   ),
                 ),
 
-                const SizedBox(height: 10), //Creates gaps between the display features
+                // Creates gaps between the display features
+                const SizedBox(height: 10),
 
                 FadeIn(
                   delay: const Duration(milliseconds: 400),
@@ -62,8 +71,9 @@ class StartPage extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: AppColors.carrotOrange,
                       foregroundColor: AppColors.fetaWhite, 
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),//how much space between the text and the edges of the button
-                      elevation: 3, //determines the strength of the buttons shadow
+                      // How much space between the text and the edges of the button
+                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      elevation: 3, // Determines the strength of the buttons shadow.
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30), // Nice rounded corners!
                       ),
